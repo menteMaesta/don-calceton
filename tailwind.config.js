bbfb7150f8a811edf5672f65d311a69eec341b12
menteMaesta <paulagomenr@gmail.com>
2024-02-12 22:34:19 -0600

    Google font, font-awesome, classnames

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Nunito"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
