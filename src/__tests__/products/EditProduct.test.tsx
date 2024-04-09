03135c197822bf2d4de4ae67d828dece7697593e
menteMaesta <paulagomenr@gmail.com>
2024-04-09 16:25:46 -0600

    Remove unused import

--- src/__tests__/products/EditProduct.test.tsx
+++ src/__tests__/products/EditProduct.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, waitFor, fireEvent } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import SnackbarProvider from "react-simple-snackbar";
 import { ROUTES } from "helpers/constants";
