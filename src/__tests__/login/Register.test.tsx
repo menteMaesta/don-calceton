6983733b11370fcb766edab22e369fba1614a688
menteMaesta <paulagomenr@gmail.com>
2024-04-16 18:08:04 -0600

    Register unit test

---
+++ src/__tests__/login/Register.test.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { render, waitFor } from "@testing-library/react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import SnackbarProvider from "react-simple-snackbar";
+import Register from "src/routes/Login/Register";
+import { ROUTES } from "src/helpers/constants";
+
+describe("Register component", () => {
+  test("renders register form", async () => {
+    const routes = [
+      {
+        path: ROUTES.REGISTER,
+        element: <Register />,
+        action: () => [],
+      },
+    ];
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: [ROUTES.REGISTER],
+    });
+
+    const { getByLabelText, getByText } = render(
+      <React.StrictMode>
+        <SnackbarProvider>
+          <RouterProvider router={router} />
+        </SnackbarProvider>
+      </React.StrictMode>
+    );
+
+    // Assert that the register form is rendered
+    const registerTitle = await waitFor(() => getByText("Registro"));
+    const nameInput = getByLabelText("Nombre");
+    const emailInput = getByLabelText("Correo");
+    const passwordInput = getByLabelText("Contraseña");
+    const saveButton = getByText("Guardar");
+
+    expect(registerTitle).toBeInTheDocument();
+    expect(nameInput).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(saveButton).toBeInTheDocument();
+  });
+});
