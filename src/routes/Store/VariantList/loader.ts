22cab0e2492a03dc03954d14bcac25fecc767833
menteMaesta <paulagomenr@gmail.com>
2024-05-03 13:01:53 -0600

    Move fetch variant items to store loader

---
+++ src/routes/Store/VariantList/loader.ts
@@ -0,0 +1,21 @@
+import Cookies from "js-cookie";
+import { getProducts } from "routes/Products/loader";
+import { fetchAllVariants } from "routes/Variants/loader";
+import { Product, VariantListItem } from "helpers/customTypes";
+
+export const fetchStorefrontData = async () => {
+  const variants = await fetchAllVariants();
+  const products = await getProducts();
+  const productOptions = products
+    .filter((product: Product) => product.variants.length > 0)
+    .map((product: Product) => ({
+      value: product.id,
+      label: product.name,
+    }));
+  return { variants, productOptions };
+};
+
+export const getCartItems = async () => {
+  const cart = JSON.parse(Cookies.get("cart") || "[]") as VariantListItem[];
+  return cart;
+};
--- src/routes/Variants/loader.ts
+++ src/routes/Variants/loader.ts
@@ -1,6 +1,4 @@
 import { LoaderFunctionArgs } from "react-router-dom";
-import { getProducts } from "routes/Products/loader";
-import { Product } from "helpers/customTypes";
 import { getVariant, getAllVariants } from "./api";
 
 export const fetchVariant = async ({ params }: LoaderFunctionArgs) => {
@@ -22,16 +20,4 @@ export const fetchAllVariants = async () => {
     throw data.errors[0];
   }
   return data;
-};
-
-export const fetchStorefrontData = async () => {
-  const variants = await fetchAllVariants();
-  const products = await getProducts();
-  const productOptions = products
-    .filter((product: Product) => product.variants.length > 0)
-    .map((product: Product) => ({
-      value: product.id,
-      label: product.name,
-    }));
-  return { variants, productOptions };
 };
